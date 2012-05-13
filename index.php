c42b60503efd592ea7b839898fbc8c4aa7b28cb0
ben2k1690 <ben2k1690@googlemail.com>
2012-05-13 11:22:55 +0200

    fixed a bug

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once('lib/StreamingWrapper.php');
 
-$stream = new Stream('In0sit', 'tribes123', Stream::METHOD_FILTER);
+$stream = new Stream('xxx', 'xxx', Stream::METHOD_FILTER);
 $stream->registerExtensionEvent("@in0sit", "cleverbot");
 //$stream->registerExtensionEvent("#chess", "chessbot");
 $stream->run();
